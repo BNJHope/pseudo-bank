260c6bf91604b3e4492be77985b2c2501cd36c72
Ben Hope <bnjhope1@gmail.com>
2023-07-16 13:04:46 +0100

    add user balance adjustments in save transaction endpoint

--- database/postgres.go
+++ database/postgres.go
@@ -3,6 +3,8 @@ package database
 import (
 	"database/sql"
 
+	"github.com/rs/zerolog/log"
+
 	"github.com/bnjhope/pseudo-bank/transaction"
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
@@ -44,13 +46,64 @@ func (tm PgTransactionsManager) GetTransactions() ([]transaction.Transaction, er
 }
 
 func (tm PgTransactionsManager) SaveTransaction(t *transaction.Transaction) (int64, error) {
-	var Id int64
-	queryDbErr := tm.db.QueryRow(`INSERT into transactions (Amount, FromAccount, ToAccount)
+	var (
+		Id                    int64
+		NewAccountFromBalance float64
+	)
+
+	tx, beginTxErr := tm.db.Begin()
+
+	if beginTxErr != nil {
+		return -1, beginTxErr
+	}
+
+	insertTransactionErr := tx.QueryRow(`INSERT into transactions (Amount, FromAccount, ToAccount)
 	VALUES ($1, $2, $3)
 	RETURNING id`, t.Amount, t.From, t.To).Scan(&Id)
 
-	if queryDbErr != nil {
-		return -1, queryDbErr
+	if insertTransactionErr != nil {
+		if rollbackErr := tx.Rollback(); rollbackErr != nil {
+			log.Fatal().Err(rollbackErr).Msg("Failed to rollback insert transaction")
+		}
+		return -1, insertTransactionErr
+	}
+
+	decrementBalanceErr := tx.QueryRow(`UPDATE users
+	SET balance = users.balance - $1
+	WHERE users.id = $2
+	RETURNING users.balance`, t.Amount, t.From).Scan(&NewAccountFromBalance)
+
+	if decrementBalanceErr != nil {
+		if rollbackErr := tx.Rollback(); rollbackErr != nil {
+			log.Fatal().Err(rollbackErr).Msg("Failed to rollback decrement balance transaction")
+		}
+		return -1, decrementBalanceErr
+	}
+
+	if NewAccountFromBalance < 0 {
+		if rollbackErr := tx.Rollback(); rollbackErr != nil {
+			log.Fatal().Err(rollbackErr).Msg("Failed to rollback from not enough funds in account")
+		}
+		return -1, &transaction.NotEnoughFundsInAccountError{
+			AccountBalance:    NewAccountFromBalance + t.Amount,
+			TransactionAmount: t.Amount,
+		}
+	}
+
+	incrementBalanceErr := tx.QueryRow(`UPDATE users
+	SET balance = users.balance + $1
+	WHERE users.id = $2
+	RETURNING users.balance`, t.Amount, t.To).Scan(&NewAccountFromBalance)
+
+	if incrementBalanceErr != nil {
+		if rollbackErr := tx.Rollback(); rollbackErr != nil {
+			log.Fatal().Err(rollbackErr).Msg("Failed to rollback increment balance transaction")
+		}
+		return -1, incrementBalanceErr
+	}
+
+	if commitErr := tx.Commit(); commitErr != nil {
+		log.Fatal().Err(commitErr).Msg("Failed to commit transaction")
 	}
 
 	return Id, nil
--- transaction/transaction.go
+++ transaction/transaction.go
@@ -1,8 +1,21 @@
 package transaction
 
+import "fmt"
+
 type Transaction struct {
 	Id     int     `json:"id"`
 	Amount float64 `json:"amount"`
 	From   []byte  `json:"from"`
 	To     []byte  `json:"to"`
+}
+
+type NotEnoughFundsInAccountError struct {
+	AccountBalance    float64
+	TransactionAmount float64
+}
+
+func (err *NotEnoughFundsInAccountError) Error() string {
+	return fmt.Sprintf(`Unable to process transaction, not enough funds in account
+	Account Balance: %f,
+	Transaction Amount: %f`, err.AccountBalance, err.TransactionAmount)
 }
