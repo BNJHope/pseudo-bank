fc2c1022cafaaf7a471d9414310a041e30252cdd
Ben Hope <bnjhope1@gmail.com>
2023-07-16 13:52:12 +0100

    add initial balance to test user

--- data/postgres/01-users.sql
+++ data/postgres/01-users.sql
@@ -7,8 +7,8 @@ CREATE TABLE users (
    balance double precision DEFAULT 0.00
 );
 
-INSERT into users (id, firstname, surname)
-VALUES ('d2e19190-59c8-4a43-8bb7-a729ea2b5173', 'Ben', 'Hope');
+INSERT into users (id, firstname, surname, balance)
+VALUES ('d2e19190-59c8-4a43-8bb7-a729ea2b5173', 'Ben', 'Hope', 40.00);
 
 INSERT into users (id, firstname, surname)
 VALUES ('1a8580b6-fb6c-4f3a-8254-3c19e638f385', 'Second', 'User');
